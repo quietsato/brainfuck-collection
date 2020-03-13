f08c8c8da87788fd50aa2d59c24cbd913d2305ec
lnsf <lnsf03@gmail.com>
2020-03-13 21:53:38 +0900

    Implementing cli

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
-mod lib;
+extern crate clap;
+use clap::{App, Arg};
 
-use lib::executor::*;
+mod lib;
+use lib::executor::Executor;
 
 fn main() {
-    // prints "Hello World."
-    // http://unidentifiedexe.hatenablog.com/entry/2019/10/10/232431
-    let mut src = String::from(
-        r#"
-        ++++++++++[->++++++++++>+++>++++++++++++>+++++>+++++++<<<<<]
-        >>>>>++.<<<<+.+++++++..+++.>++.>-.<<.+++.------.--------.>>>----.
-        "#,
-    );
+    let matches = App::new("BrainFuck Interpreter")
+        .version("1.0")
+        .author("lnsf <lnsf03@gmail.com>")
+        .about("A BrainFuck interpreter")
+        .arg(
+            Arg::with_name("Execute")
+                .short("e")
+                .long("exec")
+                .value_name("SOURCE")
+                .help("Execute passed source code")
+                .takes_value(true),
+        )
+        .arg(Arg::with_name("FILE").help("Specify source file").index(1))
+        .get_matches();
 
-    let mut e = Executor::new();
+    // exec source passed as command line argument
+    if let Some(src) = matches.value_of("SOURCE") {
+        let mut e = Executor::new();
 
-    e.append_src(src);
+        e.append_src(src.to_string());
+        e.execute().unwrap();
+    }
 
-    e.execute().ok();
+    // exec source file
+
+
+    // exec source from stdin
 }
