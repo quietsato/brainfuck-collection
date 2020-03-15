c1bc2bd0ddb9bd81df8b7ab3eac8c060937bc2be
lnsf <lnsf03@gmail.com>
2020-03-15 14:07:10 +0900

    Implement cli

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,35 @@ fn main() {
         .get_matches();
 
     // exec source passed as command line argument
-    if let Some(src) = matches.value_of("SOURCE") {
+    if let Some(src) = matches.value_of("Execute") {
         let mut e = Executor::new();
 
         e.append_src(src.to_string());
         e.execute().unwrap();
     }
-
     // exec source file
+    else if let Some(file) = matches.value_of("FILE") {
+        use std::fs::File;
+        use std::io::Read;
 
+        let mut file = File::open(file).unwrap();
+        let mut src = String::new();
+        file.read_to_string(&mut src).unwrap();
 
+        let mut e = Executor::new();
+        e.append_src(src);
+        e.execute().unwrap();
+    }
     // exec source from stdin
+    else {
+        use std::io::*;
+        let mut src = String::new();
+        stdin().read_to_string(&mut src).unwrap();
+
+
+        let mut e = Executor::new();
+        e.append_src(src);
+        e.execute().unwrap();
+    }
 }
+
